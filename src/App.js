1e8aa0ad2993ecb3ec33885dd60458303b627b32
elfin <jeonregina11@gmail.com>
2023-08-05 17:11:44 +0900

    reactMiniBlog

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import {    BrowserRouter,    Routes,    Route} from "react-router-dom";
+import { styled } from "styled-components";
+// Pages
+import MainPage from "./component/page/MainPage";
+import PostWritePage from "./component/page/PostWritePage";
+import PostViewPage from "./component/page/PostViewPage";
+
+const MainTitleText = styled.p`
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+function App(props) { 
+  return (
+    <BrowserRouter>
+      <MainTitleText>내 블로그여0</MainTitleText>
+      <Routes>
+        <Route index element={<MainPage />} />
+        <Route path="post-write" element={ <PostWritePage/>} />
+        <Route path="post/:postId" element={ <PostViewPage/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
